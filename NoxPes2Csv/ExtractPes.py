1913c68fafa46ae0b3d80aa0f14ae28941ea5115
Bjartur Thorlacius <bjt9@hi.is>
2018-02-07 22:57:46 +0000

    PES 3

--- NoxPes2Csv/ExtractPes.py
+++ NoxPes2Csv/ExtractPes.py
@@ -9,19 +9,21 @@ try:
 	labels = [];
 	
 	measurements = glob("* */*");
-        for measurement in measurements:
-            r = get_recording_with_derived(measurement);
-            signal_header = r.get_signal_header_for_signal('PES 1');
-            epochs = r.get_sleep_events();
-            data_per_epoch = [get_data_for_period(signal_header, epoch.Period) for epoch in epochs];
-            time_series = array(data_per_epoch);
-            filename = "serwatko/" + path.basename(measurement);
-            sys.stderr.write('Saving ' + filename + '.npy...');
-            save(filename, time_series);
-            sys.stderr.write(' Done.\r\nSaving ' + filename + '.txt.gz...');
-            savetxt(filename + '.txt.gz', time_series);
-            sys.stderr.write(' Done.\r\nSaving ' + filename + '.txt...');
-            savetxt(filename + '.txt', time_series);
-            sys.stderr.write(' Done.\r\n');
+	for measurement in measurements:
+	    r = get_recording_with_derived(measurement);
+	    rip_header = r.get_signal_header_for_signal('RIP Sum');
+	    breaths = r.get_breath_events(rip_header, r.analysis_period);
+	    pes_header = r.get_signal_header_for_signal('PES 3');
+	    max_pes_per_breath = array(min(get_data_for_period(pes_header, breath)) for breath in breaths);
+	    time_series = array(pes_per_breath);
+	    print(time_series.shape);
+	    filename = "serwatko/" + path.basename(measurement);
+	    sys.stderr.write('Saving ' + filename + '.npy...');
+	    save(filename, time_series);
+	    sys.stderr.write(' Done.\r\nSaving ' + filename + '.txt.gz...');
+	    savetxt(filename + '.txt.gz', time_series);
+	    sys.stderr.write(' Done.\r\nSaving ' + filename + '.txt...');
+	    savetxt(filename + '.txt', time_series);
+	    sys.stderr.write(' Done.\r\n');
 finally:
 	sys.stderr.write('--Terminating--\r\n');
