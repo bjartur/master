b8bda499a8b5186545b726ab71e22b185aec4dd5
Bjartur Thorlacius <bjt9@hi.is>
2018-04-07 13:51:37 +0000

    Tell where a list is increasing. Tested for monotonous lists.

--- csv-to-score/src/Lib.hs
+++ csv-to-score/src/Lib.hs
@@ -4,4 +4,7 @@ module Lib
     ( increasing
     ) where
 
-increasing list = list >>= const [False]
+-- Maps the first element to True. Maps a later element to True iff it is strictly greater than the previous element.
+increasing :: [Double] -> [Bool]
+increasing [] = []
+increasing list = True : zipWith (<) list (tail list)
--- csv-to-score/test/Spec.hs
+++ csv-to-score/test/Spec.hs
@@ -2,19 +2,58 @@ import Lib (increasing)
 import Test.Hspec
 import Test.QuickCheck
 import Data.Function ((&))
+import Data.List (sort)
 
-preservesListLength :: ([a] -> [a]) -> [a] -> Bool
-function `preservesListLength` list = length (function list) == length list
 
-toPreserveListLength :: (Arbitrary a, Show a) => ([a] -> [a]) -> Property
+shouldPreserveListLength = it "returns a list of length equal to the input list."
+        . toPreserveListLength
+
+toPreserveListLength :: (Arbitrary a, Show a) => ([a] -> [b]) -> Property
 toPreserveListLength = property . preservesListLength
 
-shouldPreserveListLength = it "returns a list of length equal to the input list." .
-	toPreserveListLength
+preservesListLength :: ([a] -> [b]) -> [a] -> Bool
+function `preservesListLength` list = length (function list) == length list
+
+
+shouldMapTheFirstElementToTrue = it "maps the first element, if any, to True."
+        . toMapTheFirstElementToTrue
+
+toMapTheFirstElementToTrue :: (Arbitrary a, Show a) => ([a] -> [Bool]) -> Property
+toMapTheFirstElementToTrue = property . mapsTheFirstElementToTrue
+
+mapsTheFirstElementToTrue :: ([a] -> [Bool]) -> NonEmptyList a -> Bool
+function `mapsTheFirstElementToTrue` (NonEmpty list) = head (function list)
+
+ofAscendingListShouldBeAllTrue =
+        it "returns a replicate of True for an ascending list"
+        . toBeAllTrueForAscendingList
+
+toBeAllTrueForAscendingList :: (Arbitrary a, Ord a, Show a) => ([a] -> [Bool]) -> Property
+toBeAllTrueForAscendingList = property . isAllTrueForAscendingList
+
+isAllTrueForAscendingList :: Ord a => ([a] -> [Bool]) -> [a] -> Bool
+isAllTrueForAscendingList function list =
+        function (sort list) == map (const True) list
+
+ofDescendingListShouldHaveAllFalseTail =
+        it "returns True cons a replicate of False for a descending list"
+        . property
+        . mapsDescendingTailToAllFalseTail
+
+mapsDescendingTailToAllFalseTail :: Ord a =>
+        ([a] -> [Bool]) -> NonEmptyList a -> Bool
+mapsDescendingTailToAllFalseTail function (NonEmpty list) =
+        function (reverse (sort list)) == True : map (const False) (tail list)
 
 main :: IO ()
 main = hspec $ do
-	describe "increasing" $ do
-		increasing & shouldPreserveListLength
-	describe "Three breaths each with a lower pressure than a preceding breath" $ do
-		return ()
+        describe "increasing" $ do
+                mapM_ ($ increasing) [
+                        shouldMapTheFirstElementToTrue,
+                        shouldPreserveListLength,
+                        ofAscendingListShouldBeAllTrue,
+                        ofDescendingListShouldHaveAllFalseTail
+                        ]
+                
+        describe "Three breaths each with a lower pressure than a preceding breath" $ do
+                return ()
