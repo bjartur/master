4c61da25b8e178553f50ff922916bef5036e00a1
Bjartur Thorlacius <bjt9@hi.is>
2018-04-09 00:42:51 +0000

    Nontrivial test-cases for increasing

--- csv-to-score/test/Spec.hs
+++ csv-to-score/test/Spec.hs
@@ -1,8 +1,8 @@
 import Lib (increasing)
 import Test.Hspec
-import Test.QuickCheck
+import Test.QuickCheck (Arbitrary, NonEmptyList(..), Property, property)
 import Data.Function ((&))
-import Data.List (sort)
+import Data.List (sort, nub)
 
 
 shouldPreserveListLength = it "returns a list of length equal to the input list."
@@ -33,7 +33,7 @@ toBeAllTrueForAscendingList = property . isAllTrueForAscendingList
 
 isAllTrueForAscendingList :: Ord a => ([a] -> [Bool]) -> [a] -> Bool
 isAllTrueForAscendingList function list =
-        function (sort list) == map (const True) list
+        function(sort list) == map(const True) list
 
 ofDescendingListShouldHaveAllFalseTail =
         it "returns True cons a replicate of False for a descending list"
@@ -43,17 +43,31 @@ ofDescendingListShouldHaveAllFalseTail =
 mapsDescendingTailToAllFalseTail :: Ord a =>
         ([a] -> [Bool]) -> NonEmptyList a -> Bool
 mapsDescendingTailToAllFalseTail function (NonEmpty list) =
-        function (reverse (sort list)) == True : map (const False) (tail list)
+        function(reverse(sort list)) == True : map(const False) (tail list)
+
+shouldMorphReversalIntoNegation =
+        it "preservers reversal for lists without duplicates"
+        . property
+        . morphsReversalIntoNegation
+
+morphsReversalIntoNegation :: ([Double] -> [Bool]) -> NonEmptyList Double -> Bool
+function `morphsReversalIntoNegation` (NonEmpty list) =
+        do let filtered = nub list
+           let allButFirst = tail . function
+           reverse(allButFirst filtered) == map not (allButFirst(reverse filtered))
 
 main :: IO ()
 main = hspec $ do
         describe "increasing" $ do
-                mapM_ ($ increasing) [
+                mapM_($ increasing) [
                         shouldMapTheFirstElementToTrue,
                         shouldPreserveListLength,
                         ofAscendingListShouldBeAllTrue,
-                        ofDescendingListShouldHaveAllFalseTail
+                        ofDescendingListShouldHaveAllFalseTail,
+                        shouldMorphReversalIntoNegation
                         ]
-                
+                it "returns three times True and ten times False given [1..3] ++ [2,1..(-7)]" 
+                       $         increasing([1..3] ++ [2,1..(-7)])
+                       `shouldBe`(replicate 3 True ++ replicate 10 False)
         describe "Three breaths each with a lower pressure than a preceding breath" $ do
                 return ()
