22988495612af3bb30a42d08dede06c1049b76c0
Bjartur Thorlacius <bjt9@hi.is>
2020-03-27 16:26:38 +0000

    Nox2score: use the same names as NoxPes2CSV.
    Note: This pairs a recording in Manual/ to a recording in
    ../NoxPes2CSV/nadir/BbB/ by date. This only works if there's at most one
    recording from each day.
    
    Er að þýða UCS-16 CSV skrár (sem innihalda tímasetningar á .NET formi) yfir í
    UTF-8 CSV skrár (sem innihalda tímasetningar á NumPy formi). Nú bæti ég því við
    að breyta nafninu í leiðinni. Nefnilega að láta hverja UTF-8 CSV skrá heita sama
    nafni og einhver þriðja CSV skrá sem inniheldur tímasetningar innan sömu nætur
    og CSV skráin sem ég er að þýða. Fyrstu 10 bæti þriðju skrárinnar innihalda
    byrjunardagsetningu, sem ég nota til að finna út hvort að viðkomandi skrá byrji
    sama kvöld og skráin sem ég er að þýða, og heiti þar með því nafni sem ég vil
    nota. Er að vinna með mælingar sem ég hef fengið í nokkrum útgáfum og undir
    mismunandi nöfnum.

--- Nox2score/Main.hs
+++ Nox2score/Main.hs
@@ -2,39 +2,85 @@
 module Main where
 
 import Control.Applicative (liftA2, some )
-import Control.Monad( forM_ )
+import Control.Monad( filterM, when )
 import Data.Char
+import Data.Function( (&) )
+import Data.Functor( (<&>) )
+import Data.Foldable( traverse_ )
 import Data.List
+import System.Directory( getDirectoryContents )
 import System.Environment( getArgs )
-import System.FilePath( takeFileName, (</>) )
-import System.IO( IOMode(ReadMode, WriteMode), hGetContents, hPutStr, hSetEncoding, utf16, utf8, withBinaryFile, withFile )
-import Text.ParserCombinators.ReadP( ReadP, char, eof, many, readP_to_S, satisfy, string )
+import System.Exit( die )
+import System.FilePath( FilePath, takeFileName, (</>), (<.>) )
+import System.IO( Handle, IOMode(ReadMode, WriteMode), hGetContents, hPutStr, hSetEncoding, utf16, utf8, withBinaryFile, withFile )
+import Text.ParserCombinators.ReadP( ReadP, char, eof, many, readP_to_S, satisfy, skipMany1, string )
 
 (>$):: Functor functor=> functor before-> (before-> after)-> functor after
 (>$)= flip fmap
 infixl 8 >$
 
 main:: IO ()
-main= (do
+main= do
   paths <- getArgs
   if paths `fewerThan` 2
     then mapM_ putStrLn ["Nox2score version 0", "Usage: Nox2score DESTINATION FILE..."]
-    else let destination = head paths in
-      forM_ (tail paths) $ \path->
-        withBinaryFile path ReadMode $ \inputHandle->
-         (hSetEncoding inputHandle utf16
-       >> hGetContents inputHandle
-          >$ convert
-          >>= withFile (destination </> takeFileName path) WriteMode . \output-> \outputHandle->
-                hSetEncoding outputHandle utf8
-            >> hPutStr outputHandle output)
-  )
+    else traverse_ (head paths & make) (tail paths)
 
-convert:: String-> String
+make:: FilePath-> FilePath-> IO ()
+make  destinationFolder inputPath=
+  withBinaryFile inputPath ReadMode (convertAndSave destinationFolder)
+
+convertAndSave:: FilePath-> Handle-> IO ()
+convertAndSave destinationFolder inputHandle=
+     hSetEncoding inputHandle utf16
+  >> hGetContents inputHandle >$ convert
+  >>= save destinationFolder
+
+save:: FilePath-> (DateTime, String)-> IO ()
+save destinationFolder ((DateTime year month day _ _ _), output)= do
+  stem <- recordingName year month day
+  withFile (destinationFolder </> stem <.> "csv") WriteMode (write output)
+
+write:: String-> Handle-> IO ()
+write output handle=
+      hSetEncoding handle utf8
+  >> hPutStr handle output
+
+isRecordingName:: FilePath-> Bool
+isRecordingName= readP_to_S (do
+                                     _ <- string "VSN-14-080-0"
+                                     skipMany1 (satisfy (/='_'))
+                                     _ <- string ".txt"
+                                     eof
+                                 ) <&> (not.null)
+
+recordingName:: Int-> Int-> Int-> IO FilePath
+recordingName year month day = do
+  let date = format (DateTime year month day 0 0 0) & take 10
+  entries <- getDirectoryContents "../NoxPes2Csv/nadir/BbB/"
+  let possibilities = filter isRecordingName entries :: [FilePath]
+  candidates <- filterM (startsOn date) (map ("../NoxPes2Csv/nadir/BbB/" ++) possibilities)
+  when (tail candidates & (not.null)) . die $ "No more than one recording expected on day " ++ date ++ "!"
+  let only = head candidates
+  let Just filename = stripPrefix "../NoxPes2Csv/nadir/BbB/" only
+  return $! take (length "VSN-14-080-0NN") filename
+
+startsOn:: String-> FilePath-> IO Bool
+startsOn date path= do
+  withFile path ReadMode (startsWith date)
+
+startsWith:: String-> Handle-> IO Bool
+startsWith date handle= do
+  contents <- (hGetContents handle)
+  return $! take 10 contents == date
+
+-- In: file in Noxturnal CSV format
+-- Out: (recording start datetime, file in NumPy CSV format)
+convert:: String-> (DateTime, String)
 convert= (readP_to_S file :: String-> [([(DateTime, DateTime)], String)])
   >$ last
   >$ fst
-  >$ (formatRow =<<)
+  >$ \intervals-> (intervals & head & fst, intervals >>= formatRow)
 
 file:: ReadP [ (DateTime, DateTime) ]
 file= do
