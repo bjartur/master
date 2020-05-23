db364f9e9c9f686732eaa4f4a5ab2df5285fb700
Bjartur Thorlacius <bjt9@hi.is>
2020-05-23 21:01:51 +0000

    plot: Label the x-axis, rows, and columns of heatmaps

--- plot/Main.hs
+++ plot/Main.hs
@@ -1,26 +1,29 @@
 import Control.Applicative( liftA2 )
-import Data.Colour.Names( black, blue, magenta, red, white, yellow )
+import Control.Lens.Operators( (&~) )
+import Control.Lens.Setter( (.~), (.=), mapped, over, set )
+import Control.Lens.Tuple( _1, _2 )
+import Data.Colour( Colour )
+import Data.Colour.Names( black, blue, red, white, yellow )
 import Data.Functor( (<&>) )
-import Data.Function( on )
-import Data.List( sort )
+import Data.Function( (&), on )
+import Data.List( intercalate, sort )
 import Diagrams.Backend.SVG( SVG, renderSVG )
 import Diagrams.Core.Types( Diagram )
-import Diagrams.Prelude( (&~), (.=) )
 import Diagrams.Size( dims )
 import Linear.V2( V2( V2 ) )
-import Plots.Axis( Axis, r2Axis, xAxis, xLabel, yAxis, yLabel )
+import Plots.Axis( r2Axis, xAxis, xLabel, yAxis)
 import Plots.Axis.ColourBar( colourBar )
 import Plots.Axis.Labels( tickLabelPositions )
 import Plots.Axis.Render( renderAxis )
 import Plots.Axis.Scale( axisExtend, noExtend )
 import Plots.Axis.Ticks( majorTicks, minorTicks )
-import Plots.Style( ColourMap, axisColourMap, colourMap )
+import Plots.Style( ColourMap, axisColourMap, colourMap, infColour, negInfColour )
 import Plots.Types( visible )
 import Plots.Types.HeatMap( heatMap' )
 import System.Directory( listDirectory )
-import System.FilePath( (</>), takeFileName )
+import System.FilePath( (</>), splitDirectories, takeFileName )
 
-numbers:: IO [[Double]]
+numbers:: IO [(String, [Double])]
 numbers= liftA2 (:) kao autoscores
 
 countLines:: FilePath-> IO Double
@@ -29,20 +32,20 @@ countLines= readFile <&> fmap lines <&> fmap length <&> fmap fromIntegral
 fromScoreName:: FilePath-> Double
 fromScoreName= takeFileName <&> drop (length "VSN-14-080-0") <&> take 2 <&> read
 
-autoscores:: IO [[Double]]
+autoscores:: IO [(String, [Double])]
 autoscores= traverse (tally fromScoreName) $
   ["../csv-to-score/output/"] +/+ reverse ["unabrupt", "reversal", "baseline"] +/+ reverse (map pure ['2'..'5'])
 
 fromKaoName:: FilePath-> Double
 fromKaoName= takeWhile (/='.') <&> read
 
-kao:: IO [Double]
-kao= tally fromKaoName "../Nox2score/KAO/"
+kao:: IO (String, [Double])
+kao= tally fromKaoName "../Nox2score/KAO/" <&> (_1 .~ "KAÓ")
 
 pairWith:: (a-> b)-> [a]-> [(b,a)]
 pairWith f= (map f >>= zip)
 
-tally:: (FilePath-> Double)-> FilePath-> IO [Double]
+tally:: (FilePath-> Double)-> FilePath-> IO (String, [Double])
 tally enumerate directory= do
   scorePaths <- listDirectory directory
   let enumerated = pairWith enumerate scorePaths
@@ -50,35 +53,39 @@ tally enumerate directory= do
   let filtered = filter (fst <&> forbid 13 14) enumerated
   let sorted = sort filtered
   let filenames = sorted <&> snd <&> (directory </>)
-  traverse countLines filenames
+  let label = directory & splitDirectories & ((length <&> (subtract 2)) >>= drop) & intercalate "#"
+  let values = traverse countLines filenames
+  values <&> (,) label
 
-plot:: ColourMap-> [[Double]]-> Diagram SVG
-plot colourScheme dissimilarities=
+
+plot:: Colour Double-> [(String, [Double])]-> Diagram SVG
+plot colour distributions=
   renderAxis $ r2Axis &~ do
     axisExtend .= noExtend
-    xAxis . tickLabelPositions .= [(0.5, "1")
-                                  ,(5.5, "6"{-7-})
-                                  ,(10.5, "11"{-12-})
-                                  ,(15.5, "16"{-19-})
-                                  ,(20.5, "21"{-24-})
-                                  ,(25.5, "26"{-29-})]
     xLabel .= "Participating sleeper"
+    xAxis . tickLabelPositions .= [(0.5, "#1")
+                                  ,(5.5, "#6"{-7-})
+                                  ,(10.5, "#11"{-12-})
+                                  ,(15.5, "#16"{-19-})
+                                  ,(20.5, "#21"{-24-})
+                                  ,(25.5, "#26"{-29-})]
     xAxis . majorTicks . visible .= False
     xAxis . minorTicks . visible .= False
+    yAxis . tickLabelPositions .= zip [0.5..] (distributions <&> fst)
     yAxis . majorTicks . visible .= False
     yAxis . minorTicks . visible .= False
-    axisColourMap .= colourScheme
+    axisColourMap .= colourScheme colour
     colourBar . visible .= True
-    heatMap' dissimilarities
+    heatMap' (distributions <&> snd)
 
 raw:: IO (Diagram SVG)
-raw= numbers <&> plot whites
+raw= numbers <&> plot white
 
 normalize:: [Double]-> [Double]
 normalize list= map (/ sum list) list
 
 normalized:: IO (Diagram SVG)
-normalized= numbers <&> map normalize <&> plot yellows
+normalized= numbers <&> over (mapped._2) normalize <&> plot yellow
 
 draw:: IO (Diagram SVG)-> FilePath-> IO ()
 a `draw` b= a >>= renderSVG b (dims $ V2 1000 1000.0)
@@ -88,11 +95,10 @@ main= do
   draw raw "tally.heat.svg"
   draw normalized "distribution.heat.svg"
 
-yellows:: ColourMap
-yellows= colourMap [(0,yellow), (1,red)]
-
-whites:: ColourMap
-whites= colourMap [(0,white), (1,red)]
+colourScheme:: Colour Double-> ColourMap
+colourScheme colour= colourMap [(0,colour), (1,red)]
+  & set infColour black
+  & set negInfColour blue
 
 (+/+):: [FilePath]-> [FilePath]-> [FilePath]
 (+/+)= liftA2 (</>)
