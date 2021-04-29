0f26b10439027ad191321c13ccab40cc89f7d637
Bjartur Thorlacius <bjt9@hi.is>
2021-04-29 22:24:59 +0000

    Bjartur.Time: add building blocks for histogramming overlap
    The plan is to plot a histogram where each bin is a number between 0 and 15 (the
    number of patterns). The height of each bar will be how much sleep either
    matches precisely that many patterns or matches all but that many patterns.

--- lib/src/Bjartur/Time.hs
+++ lib/src/Bjartur/Time.hs
@@ -2,6 +2,7 @@ module Bjartur.Time where
 
 import Data.Function( (&) )
 import Data.Functor( (<&>) )
+import Data.List( foldl1' )
 import Test.QuickCheck ( Arbitrary, arbitrary, choose )
 import qualified Data.Interval     as Interval
 import qualified Data.IntervalSet  as IntervalSet
@@ -49,3 +50,44 @@ measure :: Interval -> Int
 measure i = let (Finite lower) = Interval.lowerBound i
                 (Finite upper) = Interval.upperBound i
             in measure' (lower, upper)
+
+flatten :: [(DateTime, DateTime)]-> [DateTime]
+flatten [] = []
+flatten ((a,b):xs) = a:b:flatten xs
+
+merge :: [DateTime]-> [DateTime]-> [DateTime]
+merge [] bs = bs
+merge as [] = as
+merge (a:as) (b:bs) = merge' a as b bs (compare a b)
+
+merge' :: DateTime-> [DateTime]-> DateTime-> [DateTime]-> Ordering-> [DateTime]
+merge' a as _ bs EQ = a:merge as bs
+merge' a as b bs LT = a:merge as (b:bs)
+merge' a as b bs GT = b:merge (a:as) bs
+
+mergeAll :: [[DateTime]]-> [DateTime]
+mergeAll [] = []
+mergeAll [x] = x
+mergeAll (as:bs:rest) = mergeAll ((merge as bs):rest)
+
+flattenAll :: [[(DateTime,DateTime)]] -> [DateTime]
+flattenAll xs = map flatten xs & mergeAll
+
+telja :: [DateTime]-> [(DateTime, DateTime)]-> [Bool]
+telja [] [] = []
+telja [] datetime = error $ "DateTime " ++ show datetime ++ " out of bounds!"
+telja (_:allir) [] = False : telja allir []
+telja (núverandi:allir) ((upphaf,endir):eini) = if núverandi < upphaf then False : telja allir ((upphaf,endir):eini) else True : teljaTrue allir ((upphaf,endir):eini)
+
+
+teljaTrue :: [DateTime]-> [(DateTime, DateTime)]-> [Bool]
+teljaTrue [] _ = error "Closing datetime lost!"
+teljaTrue _ [] = error "teljaTrue called without the open interval!"
+teljaTrue (núverandi:allir) ((_,endir):eini) = if núverandi < endir then True : teljaTrue allir ((undefined,endir):eini) else False : telja allir eini
+
+samtelja :: [[(DateTime,DateTime)]]-> [Int]
+samtelja [] = []
+samtelja xs =  do
+  let u = flattenAll xs & telja
+  let v = map u xs ::[[Bool]]
+  v <&> map fromEnum & foldl1' (zipWith (+))
--- lib/test/TypesSpec.hs
+++ lib/test/TypesSpec.hs
@@ -1,10 +1,12 @@
 module TypesSpec ( spec ) where
 
 import Data.Function ( (&) )
+import Data.Functor( (<&>) )
+import Data.List( sort )
 import Test.Hspec
-import Test.QuickCheck ( property )
+import Test.QuickCheck ( Gen, SortedList(getSorted), arbitrary, elements, forAll, getSize, listOf, listOf1, property, vectorOf )
 
-import Bjartur.Types
+import Bjartur.Time
 
 spec :: Spec
 spec = do
@@ -19,3 +21,41 @@ spec = do
                                 then False
                                 else head accessors datetime < 0
         property $ \a b-> (a < b) == (a `lessThan` b)
+    describe "samtelja" $ do
+      it "preserves length" $ do
+        forAll (listOf arbitraryDateTimes) $ \periodLists->
+          length (samtelja periodLists) >= (map length periodLists & sum)
+      it "counts each period at least once" $ do
+        forAll (listOf arbitraryDateTimes) $ \periodLists->
+          sum (samtelja periodLists) >= (map length periodLists & sum)
+    describe "telja" $ do
+      it "preserves the length of the flattened list of datetimes" $ do
+        forAll (listOf1 arbitraryDateTimes) $ \periodLists->
+          forAll (elements periodLists) $ \periodList-> do
+            let flattened = flattenAll periodLists
+            length(telja flattened periodList) `shouldBe` length flattened
+    describe "mergeAll" $ do
+      it "returns an ascending list if each input is ascending" $ do
+        forAll (arbitrary ::Gen [SortedList DateTime]) $ \wrapped-> do
+          let lists = map getSorted wrapped
+          mergeAll lists `shouldBe` (concat lists & sort)
+
+
+nubSort:: [DateTime]-> [DateTime]
+nubSort = sort <&> fastnub
+    where fastnub(one:other:rest)= if one==other then fastnub(one:rest) else one:fastnub(other:rest)
+          fastnub(short)= short
+
+arbitraryDateTimes:: Gen [(DateTime,DateTime)]
+arbitraryDateTimes= do
+      size<- getSize
+      mo <- arbitrary ::Gen Int
+      yr <- arbitrary ::Gen Int
+      datetimes<- vectorOf (2 * abs size + 2) arbitrary
+        <&> map (\datetime-> datetime{month = mo, year = yr })
+        <&> nubSort ::Gen[DateTime]
+      let pair[]= []
+          pair[_]= []
+          pair(a:b:xs)= (a, b) : pair xs
+      let disjoints= pair datetimes
+      return $! disjoints
